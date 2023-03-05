12e48f68651aedf99676fdcecc5d35700631b1db
vitorreao <vitorreao@pm.me>
2023-03-05 12:06:12 -0300

    Simplify Player script

--- character_controller/player.gd
+++ character_controller/player.gd
@@ -8,24 +8,15 @@ signal navigation_finished()
 
 @onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
 
-var _is_navigating: bool = false
-var _curr_dest_position: Vector3 = Vector3.ZERO
-var _curr_src_rotation: float
-var _curr_dest_rotation: float
-
 func _on_new_nav_target(node: NavArea, position: Vector3):
 	navigation_agent.set_target_position(position)
 	navigation_agent.target_desired_distance = node.target_desired_distance
 	navigation_agent.path_desired_distance = node.path_desired_distance
 
 func _physics_process(delta):
-	if navigation_agent.is_navigation_finished():
-		if _is_navigating: # was navigating
-			navigation_finished.emit()
-		_is_navigating = false
-		return
-	_set_new_velocity()
-	_look_at_target(delta)
+	if not navigation_agent.is_navigation_finished():
+		_set_new_velocity()
+		_look_at_target(delta)
 
 func _look_at_target(delta):
 	var next_path_position : Vector3 = navigation_agent.get_next_path_position()
@@ -37,15 +28,14 @@ func _look_at_target(delta):
 
 func _set_new_velocity():
 	var next_path_position : Vector3 = navigation_agent.get_next_path_position()
-	var current_agent_position : Vector3 = global_transform.origin
-	var new_velocity : Vector3 = next_path_position - current_agent_position
-	new_velocity = new_velocity.normalized()
-	new_velocity = new_velocity * movement_speed
-	if new_velocity.length() > 0:
-		_is_navigating = true
+	var new_velocity : Vector3 = next_path_position - global_transform.origin
+	new_velocity = new_velocity.normalized() * movement_speed
 	navigation_agent.set_velocity(new_velocity)
 
 func _on_NavigationAgent3D_velocity_computed(safe_velocity : Vector3):
 	# Move CharacterBody3D with the computed `safe_velocity` to avoid dynamic obstacles.
 	set_velocity(safe_velocity)
 	move_and_slide()
+
+func _on_navigation_agent_3d_navigation_finished():
+	navigation_finished.emit()
