9ab78f75dba62ffcd686d7c6deb9e94e3a96152d
vitorreao <vitorreao@pm.me>
2023-03-05 16:34:39 -0300

    Fix camera rotation bug
    There was a bug in the camera rotation script, where it would spin
    infinitely when a single rotation move would extrapolate -180 or 180
    degrees.
    
    Instead of using `rotation_degrees`, we are now using Quaternions and
    its built-in function for interpolation. It fixed the bug.

--- character_controller/camera_movement.gd
+++ character_controller/camera_movement.gd
@@ -4,7 +4,7 @@ extends Camera3D
 @export var distance_to_target: float = 9
 @export var sensitivity: float = 0.01
 @export var starting_pitch: float = -50
-@export var lerp_speed: float = 3.0
+@export var rotation_speed: float = 2.0
 
 var _start_mouse_position: Vector2
 var _axis: Vector2
@@ -25,11 +25,14 @@ func _process(delta):
 		_axis = get_viewport().get_mouse_position() - _start_mouse_position
 	if Input.is_action_just_released("Orbit"):
 		_axis = Vector2.ZERO
+	if Input.is_action_just_released("Zoom In"):
+		pass
 
 func _physics_process(delta):
 	_yaw -= _axis.x * sensitivity
 	_pitch -= _axis.y * sensitivity
 	_pitch = clamp(_pitch, -85, -20)
-	var target_rotation = Vector3(_pitch, _yaw, 0)
-	rotation_degrees = rotation_degrees.lerp(target_rotation, lerp_speed * delta)
+	var target_rotation = Quaternion.from_euler(Vector3(deg_to_rad(_pitch), deg_to_rad(_yaw), 0))
+	var new_rotation = Quaternion(transform.basis).slerp(target_rotation, rotation_speed * delta)
+	transform.basis = Basis(new_rotation)
 	global_position = target.global_position + global_transform.basis.z * distance_to_target
