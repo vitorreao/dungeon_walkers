cd3c12a957b22fc55021c03230a71f892e96b9ba
vitorreao <vitorreao@pm.me>
2023-03-05 17:48:33 -0300

    Movement target now spins

--- character_controller/player_movement.gd
+++ character_controller/player_movement.gd
@@ -8,8 +8,7 @@ var _current_target_position: Vector3 = Vector3.ZERO
 var _has_current_target_position: bool = false
 
 func _set_nav_target(node: CollisionObject3D, position: Vector3):
-	$Target.set_global_position(position)
-	$Target.set_visible(true)
+	_show_target_at(position)
 	new_nav_target.emit(node, position)
 
 func _physics_process(delta):
@@ -17,7 +16,7 @@ func _physics_process(delta):
 		_set_nav_target(_current_target_node, _current_target_position)
 
 func _on_player_reached_target():
-	$Target.set_visible(false)
+	_hide_target()
 	_current_target_position = Vector3.ZERO
 	_current_target_node = null
 	_has_current_target_position = false
@@ -34,3 +33,12 @@ func _on_target_mouse_motion(
 	_current_target_position = position
 	_current_target_node = node
 	_has_current_target_position = true
+
+func _show_target_at(position: Vector3):
+	$Target.set_global_position(position)
+	$Target.set_visible(true)
+	$Target/Arrow/AnimationPlayer.play("spin")
+
+func _hide_target():
+	$Target/Arrow/AnimationPlayer.stop()
+	$Target.set_visible(false)
