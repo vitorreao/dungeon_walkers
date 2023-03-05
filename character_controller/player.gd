5f818240b8302b30ffa42ef8fa589a0343cb4beb
vitorreao <vitorreao@pm.me>
2023-03-05 20:55:54 -0300

    Refactor target desired distance
    This diff uses the `target_desired_distance` variable from `NavTarget`
    to set a padding between the Player character and its interactable
    targets. Targets on the ground will not have this padding applied.
    
    I have later decided to drop this idea and just use collision shapes
    to avoid having a player character stand awkwardly close to a target
    enemy character. This diff is moved to a separate branch in case I find
    another use for this code.

--- character_controller/player.gd
+++ character_controller/player.gd
@@ -8,21 +8,26 @@ signal navigation_finished()
 
 @onready var navigation_agent : NavigationAgent3D = $NavigationAgent3D
 
+var _target: NavTarget
+var _target_position: Vector3
+
 func _on_new_nav_target(node: NavTarget, position: Vector3):
 	navigation_agent.set_target_position(position)
 	navigation_agent.target_desired_distance = node.target_desired_distance
 	navigation_agent.path_desired_distance = node.path_desired_distance
+	_target = node
+	_target_position = position
 
 func _physics_process(delta):
-	if not navigation_agent.is_navigation_finished():
+	if not _should_stop_navigating():
 		_set_new_velocity()
 	_look_at_target(delta)
 
 func _look_at_target(delta):
-	var next_path_position : Vector3 = navigation_agent.get_next_path_position()
+	var target_position = _get_target_position()
 	var target_rotation_y = atan2(
-		global_position.x - next_path_position.x,
-		global_position.z - next_path_position.z
+		global_position.x - target_position.x,
+		global_position.z - target_position.z
 	)
 	rotation.y = lerp_angle(rotation.y, target_rotation_y, turn_speed * delta)
 
@@ -39,3 +44,18 @@ func _on_NavigationAgent3D_velocity_computed(safe_velocity : Vector3):
 
 func _on_navigation_agent_3d_navigation_finished():
 	navigation_finished.emit()
+
+func _get_target_position():
+	if not navigation_agent.is_navigation_finished():
+		return navigation_agent.get_next_path_position()
+	if _target != null:
+		return _target_position
+	return global_position
+
+func _should_stop_navigating():
+	if navigation_agent.is_navigation_finished():
+		return true
+	if navigation_agent.is_target_reached() and _target.interactable:
+		navigation_agent.set_target_position(global_position)
+		return true
+	return false
